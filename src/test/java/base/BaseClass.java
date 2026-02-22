498bf9e08f1dcc35da89164c7aac2075cdf32ed0
SumilBajarang <136956674+SumilBajarang@users.noreply.github.com>
2026-02-22 11:24:10 +0530

    Create reusable base, page objects, and cucumber steps

--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -4,14 +4,31 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class BaseClass {
-    public WebDriver driver;
 
-    public void setup() {
-        this.driver = new ChromeDriver();
-        this.driver.manage().window().maximize();
-        this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    protected WebDriver driver;
+
+    protected void initializeDriver() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
+    protected void openApplication(String url) {
+        driver.get(url);
+    }
+
+    protected void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
