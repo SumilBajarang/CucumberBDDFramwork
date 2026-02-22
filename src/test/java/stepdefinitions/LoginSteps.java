b78e8864a0dd3d5de6d9c71c255a29b71ce9a5bf
SumilBajarang <136956674+SumilBajarang@users.noreply.github.com>
2026-02-22 11:43:39 +0530

    Fix Cucumber step definition inheritance violation

--- src/test/java/stepdefinitions/LoginSteps.java
+++ src/test/java/stepdefinitions/LoginSteps.java
@@ -2,11 +2,32 @@ package stepdefinitions;
 
 import org.testng.Assert;
 
+import base.BaseClass;
+import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import pages.HomePage;
+import pages.LoginPage;
 
-public class LoginSteps extends BaseSteps {
+public class LoginSteps extends BaseClass {
+
+    private LoginPage loginPage;
+    private HomePage homePage;
+
+    private void launchApplication() {
+        initializeDriver();
+        openApplication("https://demo.nopcommerce.com/");
+
+        loginPage = new LoginPage(driver);
+        homePage = new HomePage(driver);
+    }
+
+    @Given("the user initializes browser")
+    @Given("uuser initializes browser")
+    public void theUserInitializesBrowser() {
+        launchApplication();
+    }
 
     @Given("the user is on the nopCommerce login page")
     public void navigateToLoginPage() {
@@ -35,5 +56,10 @@ public class LoginSteps extends BaseSteps {
     @Then("the user should see a welcome message")
     public void theUserShouldSeeAWelcomeMessage() {
         Assert.assertTrue(homePage.isWelcomeTextDisplayed(), "Welcome message is not displayed.");
+    }
+
+    @After
+    public void tearDown() {
+        closeBrowser();
     }
 }
